046344b6460f6fe3814b592bebeaeca755072155
Mickael DESHAIES <m.deshaies04@gmail.com>
2020-05-06 10:31:07 +0200

    Initialisation Projet

---
+++ BierBuyFR/BierBuyFR.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BierBuyFR.Web.MvcApplication" Language="C#" %>
---
+++ BierBuyFR/BierBuyFR.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BierBuyFR.Web.Startup))]
+namespace BierBuyFR.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
